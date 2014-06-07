f5602ab377d6d1c580342a86b08d3c4767cd4610
minggo620 <1053200192@qq.com>
2014-06-07 18:53:24 +0800

    天剑百度广告监听器

---
+++ battery_notification/src/minggo/battery/listener/BaiduAdListener.java
@@ -0,0 +1,69 @@
+package minggo.battery.listener;
+
+import org.json.JSONObject;
+
+import com.baidu.mobads.AdView;
+import com.baidu.mobads.AdViewListener;
+/**
+ * 百度广告适配器
+ * @author minggo
+ * @time 2014-6-7 S下午6:07:24
+ */
+public abstract class BaiduAdListener implements AdViewListener {
+
+	@Override
+	public void onAdClick(JSONObject arg0) {
+
+	}
+
+	@Override
+	public void onAdFailed(String arg0) {
+
+	}
+
+	@Override
+	public void onAdReady(AdView arg0) {
+
+	}
+
+	@Override
+	public void onAdShow(JSONObject arg0) {
+
+	}
+
+	@Override
+	public void onAdSwitch() {
+
+	}
+
+	@Override
+	public void onVideoClickAd() {
+
+	}
+
+	@Override
+	public void onVideoClickClose() {
+
+	}
+
+	@Override
+	public void onVideoClickReplay() {
+
+	}
+
+	@Override
+	public void onVideoError() {
+
+	}
+
+	@Override
+	public void onVideoFinish() {
+
+	}
+
+	@Override
+	public void onVideoStart() {
+
+	}
+
+}
---
+++ battery_notification/src/minggo/battery/listener/BaiduAdListenerAdapter.java
@@ -0,0 +1,22 @@
+package minggo.battery.listener;
+
+import com.baidu.mobads.AdView;
+/**
+ * 百度广告适配器
+ * @author minggo
+ * @time 2014-6-7 S下午6:09:30
+ */
+public class BaiduAdListenerAdapter extends BaiduAdListener{
+
+	@Override
+	public void onAdReady(AdView arg0) {
+		super.onAdReady(arg0);
+		
+	}
+
+	@Override
+	public void onVideoClickAd() {
+		super.onVideoClickAd();
+	}
+	
+}
