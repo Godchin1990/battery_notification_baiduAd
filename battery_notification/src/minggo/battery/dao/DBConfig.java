dd00f7989db97ffafdfb9929131a5fe90770ecea
daitongmin <daitongmin@PC3GD000753.jiubang.org>
2014-09-11 15:37:25 +0800

    1.添加闹钟数据库。 2.声明闹钟广播。

--- battery_notification/src/minggo/battery/dao/DBConfig.java
+++ battery_notification/src/minggo/battery/dao/DBConfig.java
@@ -10,7 +10,7 @@ public class DBConfig {
 	// 数据库名称
 	public static final String NAME = "minggo.battery";
 	// 数据库版本
-	public static final int VERSION = 1;
+	public static final int VERSION = 2;
 	/**
 	 * 录音保存数据库
 	 */
@@ -19,4 +19,8 @@ public class DBConfig {
 	 * 用户数据库
 	 */
 	public static final String TABLE_USER = "t_user";
+	/**
+	 * 闹钟数据表
+	 */
+	public static final String TABLE_ALARM = "t_alarmer";
 }
--- battery_notification/src/minggo/battery/dao/DbOpenHelper.java
+++ battery_notification/src/minggo/battery/dao/DbOpenHelper.java
@@ -1,7 +1,11 @@
 package minggo.battery.dao;
 
+import java.util.List;
+
+import minggo.battery.model.Alarmer;
 import minggo.battery.model.SoundRecord;
 import minggo.battery.model.User;
+import minggo.battery.util.UserUtil;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -12,7 +16,8 @@ import android.database.sqlite.SQLiteOpenHelper;
  * @time 2014-6-23 S下午9:42:46
  */
 public class DbOpenHelper extends SQLiteOpenHelper {
-
+	
+	
 	public DbOpenHelper(Context context) {
 		super(context,DBConfig.NAME, null, DBConfig.VERSION);
 	}
@@ -22,11 +27,15 @@ public class DbOpenHelper extends SQLiteOpenHelper {
 		//创建录音表
 		db.execSQL(SQLHelper.getCreateTable(DBConfig.TABLE_SOUND_RECORD,SoundRecord.class));
 		db.execSQL(SQLHelper.getCreateTable(DBConfig.TABLE_USER,User.class));
+		db.execSQL(SQLHelper.getCreateTable(DBConfig.TABLE_ALARM,Alarmer.class));
 		
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		if (oldVersion==1&&newVersion==2) {
+			db.execSQL(SQLHelper.getCreateTable(DBConfig.TABLE_ALARM,Alarmer.class));
+		}
 		
 	}
 
