8f363fd6891dd8ea574c4bea68dc3d3639c8e3cc
daitongmin <daitongmin@PC3GD000753.jiubang.org>
2014-09-12 18:20:56 +0800

    修复自定义录音报时bug

--- battery_notification/src/minggo/battery/reciever/TimeChangeReciever.java
+++ battery_notification/src/minggo/battery/reciever/TimeChangeReciever.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import minggo.battery.model.SoundRecord;
 import minggo.battery.model.User;
-import minggo.battery.service.MinggoApplication;
 import minggo.battery.util.MinggoDate;
 import minggo.battery.util.PlaySound;
 import minggo.battery.util.PreferenceShareUtil;
@@ -28,37 +27,45 @@ public class TimeChangeReciever extends BroadcastReceiver {
 	private MinggoDate date;
 	private User user;
 	private Vibrator vibrator;
-	
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		String action = intent.getAction();
-		
+
 		if (action.equals(Intent.ACTION_TIME_TICK)) {
-			
 			// System.out.println("整点报时--->"+date.get24Hour()+"点");
-			if (PreferenceShareUtil.getZhengTimeFlag(context)) {
-				if (vibrator==null) {
+			if (PreferenceShareUtil.getZhengTimeFlag(context) && !PreferenceShareUtil.getDefineSoundFlag(context)) {
+				if (vibrator == null) {
 					vibrator = (Vibrator) context.getSystemService(Service.VIBRATOR_SERVICE);
 				}
-				
 				date = new MinggoDate();
-				user = UserUtil.getUser(context, MinggoApplication.EMAIL);
-				if (user.type == 0) {
-					if (date.getMinutes() == 00 && date.get24Hour() > 5) {
+				if (date.getMinutes() == 00 && date.get24Hour() > 5) {
+					if (PreferenceShareUtil.getShockFlag(context)) {
 						vibrator.vibrate(500);
-						// System.out.println("整点报时--->"+date.get24Hour()+"点");
-						try {
-							PlaySound.play("sound/" + date.get24Hour() + ".mp3", context.getAssets());
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
 					}
-				} else if (user.type == 1) {
+					// System.out.println("整点报时--->"+date.get24Hour()+"点");
+					try {
+						PlaySound.play("sound/" + date.get24Hour() + ".mp3", context.getAssets());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+			} else if (PreferenceShareUtil.getZhengTimeFlag(context) && PreferenceShareUtil.getDefineSoundFlag(context)) {
+				if (vibrator == null) {
+					vibrator = (Vibrator) context.getSystemService(Service.VIBRATOR_SERVICE);
+				}
+
+				date = new MinggoDate();
+				user = UserUtil.getFirstUser(context);
+				if (user != null && user.type == 1) {
 					if (date.getMinutes() == 00) {
 						SoundRecord soundRecord = SoundRecordUtil.getSoundRecord(context, date.get24Hour());
 
 						if (soundRecord != null) {
-							vibrator.vibrate(500);
+							if (PreferenceShareUtil.getShockFlag(context)) {
+								vibrator.vibrate(500);
+							}
 							try {
 								PlaySound.playVoice2(soundRecord.path, context.getResources().getAssets(), 2);
 							} catch (IOException e) {
@@ -66,11 +73,12 @@ public class TimeChangeReciever extends BroadcastReceiver {
 							}
 						} else {
 							if (date.getMinutes() == 00 && date.get24Hour() > 5) {
-								vibrator.vibrate(500);
+								if (PreferenceShareUtil.getShockFlag(context)) {
+									vibrator.vibrate(500);
+								}
 								// System.out.println("整点报时--->"+date.get24Hour()+"点");
 								try {
-									PlaySound.play("sound/" + date.get24Hour() + ".mp3",
-											context.getAssets());
+									PlaySound.play("sound/" + date.get24Hour() + ".mp3", context.getAssets());
 								} catch (IOException e) {
 									e.printStackTrace();
 								}
@@ -78,7 +86,7 @@ public class TimeChangeReciever extends BroadcastReceiver {
 						}
 					}
 				}
-			}else{
+			} else {
 				Log.i("alert", "设置了不提醒");
 			}
 		}
