217eafd9fd961a498036d15602efee60784189c7
minggo620 <1053200192@qq.com>
2014-09-13 21:18:29 +0800

    完成闹钟的震动和声音报时

--- battery_notification/src/minggo/battery/activity/AlarmWakeActivity.java
+++ battery_notification/src/minggo/battery/activity/AlarmWakeActivity.java
@@ -1,12 +1,17 @@
 package minggo.battery.activity;
 
+import java.io.IOException;
 import java.util.Calendar;
 
 import minggo.battery.R;
 import minggo.battery.model.Alarmer;
 import minggo.battery.reciever.AlarmerReciever;
+import minggo.battery.util.PlaySound;
+import minggo.battery.util.PreferenceShareUtil;
 import android.app.Activity;
+import android.app.Service;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
@@ -30,6 +35,9 @@ public class AlarmWakeActivity extends Activity implements OnClickListener{
 	private TextView timeTv;
 	private TextView dayTv;
 	private Alarmer alarmer;
+	
+	private Vibrator vibrator;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); // 全屏
@@ -57,31 +65,46 @@ public class AlarmWakeActivity extends Activity implements OnClickListener{
 	}
 	
 	private void initData(){
-		
-		switch (alarmer.type) {
-		case AlarmerReciever.DEFINE_ALARM:
-			themeIv.setImageResource(R.drawable.emoji_298);
-			break;
-		case  AlarmerReciever.DRINK_ALARM:
-			themeIv.setImageResource(R.drawable.smiley_60);
-			break;
-		case AlarmerReciever.BIRTHDAY_ALARM:
-			themeIv.setImageResource(R.drawable.emoji_103);
-			break;
-		default:
-			break;
+		if (alarmer!=null) {
+			
+			switch (alarmer.type) {
+			case AlarmerReciever.DEFINE_ALARM:
+				themeIv.setImageResource(R.drawable.emoji_298);
+				break;
+			case  AlarmerReciever.DRINK_ALARM:
+				themeIv.setImageResource(R.drawable.smiley_60);
+				break;
+			case AlarmerReciever.BIRTHDAY_ALARM:
+				themeIv.setImageResource(R.drawable.emoji_103);
+				break;
+			default:
+				break;
+			}
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTimeInMillis(alarmer.alarmTime);
+			if (calendar.get(Calendar.MINUTE)<10) {
+				timeTv.setText(calendar.get(Calendar.HOUR_OF_DAY)+":0"+calendar.get(Calendar.MINUTE));
+			}else{
+				timeTv.setText(calendar.get(Calendar.HOUR_OF_DAY)+":"+calendar.get(Calendar.MINUTE));
+			}
+			dayTv.setText(calendar.get(Calendar.YEAR)+"-"+(calendar.get(Calendar.MONTH)+1)+"-"+(calendar.get(Calendar.DAY_OF_MONTH)));	
+			titleTv.setText(alarmer.title);
+			
+			if (alarmer.shock==1) {
+				if (vibrator == null) {
+					vibrator = (Vibrator) this.getSystemService(Service.VIBRATOR_SERVICE);
+				}
+				//vibrator.vibrate(500);
+				vibrator.vibrate(new long[]{650,650}, 0);
+			}
+			if (alarmer.sound==1) {
+				try {
+					PlaySound.playDefineAlarmVoice2(alarmer.soundPath, this.getAssets(), 1, true);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTimeInMillis(alarmer.alarmTime);
-		if (calendar.get(Calendar.MINUTE)<10) {
-			timeTv.setText(calendar.get(Calendar.HOUR_OF_DAY)+":0"+calendar.get(Calendar.MINUTE));
-		}else{
-			timeTv.setText(calendar.get(Calendar.HOUR_OF_DAY)+":"+calendar.get(Calendar.MINUTE));
-		}
-		dayTv.setText(calendar.get(Calendar.YEAR)+"-"+(calendar.get(Calendar.MONTH)+1)+"-"+(calendar.get(Calendar.DAY_OF_MONTH)));	
-		titleTv.setText(alarmer.title);
-		
-		
 		
 	}
 	
@@ -98,6 +121,8 @@ public class AlarmWakeActivity extends Activity implements OnClickListener{
 	@Override
 	public void onBackPressed() {
 		super.onBackPressed();
+		vibrator.cancel();
+		PlaySound.stopVoice();
 	}
 
 	@Override
